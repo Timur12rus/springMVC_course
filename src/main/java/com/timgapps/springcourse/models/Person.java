a0681718ee5ce134223e6aaf3f3ee3f8922ce6f8
tim.gordeev2015@yandex.ru <tim.gordeev2015@yandex.ru>
2023-04-28 16:56:26 +0300

    Валидация форм. Аннотация @Valid.(start)

--- src/main/java/com/timgapps/springcourse/models/Person.java
+++ src/main/java/com/timgapps/springcourse/models/Person.java
@@ -4,12 +4,18 @@ public class Person {
     private int id;
     private String name;
 
+    private int age;
+
+    private String email;
+
     public Person() {
     }
 
-    public Person(int id, String name) {
+    public Person(int id, String name, int age, String email) {
         this.id = id;
         this.name = name;
+        this.age = age;
+        this.email = email;
     }
 
     public int getId() {
@@ -18,6 +24,22 @@ public class Person {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getName() {

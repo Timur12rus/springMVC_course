222b8ab6fcc0e5a90eab19f8c82b2f97d5f4a4d9
tim.gordeev2015@yandex.ru <tim.gordeev2015@yandex.ru>
2023-05-01 14:16:51 +0300

    Валидация форм. Аннотация @Valid (finished lesson)

--- src/main/java/com/timgapps/springcourse/cotrollers/PeopleController.java
+++ src/main/java/com/timgapps/springcourse/cotrollers/PeopleController.java
@@ -5,7 +5,10 @@ import com.timgapps.springcourse.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/people")
@@ -40,7 +43,11 @@ public class PeopleController {
     }
 
     @PostMapping()
-    public String create(@ModelAttribute("person") Person person) {
+    public String create(@ModelAttribute("person") @Valid Person person, // если ошибка, то она передается в оъект bindingResult
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "people/new";
+        }
         personDAO.save(person);
         return "redirect:/people";
     }
@@ -52,7 +59,12 @@ public class PeopleController {
     }
 
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+    public String update(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+        if (bindingResult.hasErrors()) {
+            return "people/edit";
+        }
         personDAO.update(id, person);
         return "redirect:/people";
     }

c6922c328cef2652c6a99acb037a22b3cb7a22a9
Timur <tim.gordeev2015@yandex.ru>
2023-05-03 00:25:28 +0300

    JDBC API. Базы данных (start)

--- src/main/java/com/timgapps/springcourse/dao/PersonDAO.java
+++ src/main/java/com/timgapps/springcourse/dao/PersonDAO.java
@@ -1,33 +1,60 @@
 package com.timgapps.springcourse.dao;
 
-import com.timgapps.springcourse.cotrollers.PeopleController;
 import com.timgapps.springcourse.models.Person;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class PersonDAO {
     private static int PEOPLE_COUNT;
-    private List<Person> people;
+    private static final String URL = "jdbc:postgresql://localhost:5432/first_db";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "postgres";
+    private static Connection connection;
 
-    {
-        people = new ArrayList<>();
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
 
-        people.add(new Person(++PEOPLE_COUNT, "Tom", 24, "tom@mail.ru"));
-        people.add(new Person(++PEOPLE_COUNT, "Bob", 52, "bob@mail.ru"));
-        people.add(new Person(++PEOPLE_COUNT, "Mike", 18, "mike@yahoo.com"));
-        people.add(new Person(++PEOPLE_COUNT, "Katy", 34, "katy@mail.com"));
+        try {
+            connection = DriverManager.getConnection(URL,
+                    USERNAME,
+                    PASSWORD);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     public List<Person> index() {
+        List<Person> people = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement(); // тот объект, который содержит в себе запрос к базе данных
+            String SQL = "SELECT * FROM Person";
+            ResultSet resultSet = statement.executeQuery(SQL);    // передаем наш SQL запрос, в этом объекте лежат наши строки
+
+            while(resultSet.next()) {
+                Person person = new Person();
+
+                person.setId("id");
+                person.setName(resultSet.getString());
+                person.setName(resultSet.getString());
+                person.setName(resultSet.getString());
+
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return people;
     }
 
     public Person show(int id) {
-        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+//        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
     }
 
     public void save(Person person) {
@@ -36,11 +63,11 @@ public class PersonDAO {
     }
 
     public void update(int id, Person updatedPerson) {
-        Person personToBeUpdated = show(id);
-        personToBeUpdated.setName(updatedPerson.getName());
+//        Person personToBeUpdated = show(id);
+//        personToBeUpdated.setName(updatedPerson.getName());
     }
 
     public void delete(int id) {
-        people.removeIf(p -> p.getId() == id);
+//        people.removeIf(p -> p.getId() == id);
     }
 }
